65fc135efeb6c7f501e81339925fb290e0274332
Julien Maury <jmaury@beapi.fr>
2018-12-08 00:21:42 +0100

    ignition

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# jm-twitter-cards-refresh-image
+# JM Twitter Cards Refresh Image
+
 When Twitter fetches Twitter image it keeps it in cache, so this plugin filters JM Twitter Cards to add a query string to force cache busting.
+Props to https://www.contentkingapp.com/ for the Idea.
---
+++ jm-twitter-cards-refresh-image.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: JM Twitter Cards Refresh image
+Plugin URI: https://julien-maury.com
+Description: this plugin filters JM Twitter Cards to add a query string to force cache busting on image meta.
+Author: Julien Maury
+Author URI: https://julien-maury.com
+Version: 1.0
+License: GPL2++
+
+JM Twitter Cards Plugin
+Copyright (C) 2016, Julien Maury - contact@julien-maury.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Add some security, no direct load !
+defined( 'ABSPATH' )
+	or die( 'No direct load !' );
+
+add_filter( 'jm_tc_image_source', 'jm_tc_refresh_image' );
+function jm_tc_refresh_image( $image ) {
+
+	$params = (array) apply_filters( 'jm_tc_refresh_image_query_string_params', [ 'tc' => strtotime( 'now' ) ], $image );
+
+	return add_query_arg( $params, $image );
+}
